f35c5af1c04cc5aa91e672b7499964e0c9082ef4
Abhi Agarwal <goldcobrapro@gmail.com>
2017-04-19 18:04:18 -0400

    Start of Project 22

---
+++ Source/Project 022/Project22.cpp
@@ -0,0 +1,12 @@
+// Copyright 2017 <Abhi Agarwal>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> readNamesFromFiles() {
+  
+}
+
+int main() {
+  return 0;
+}
