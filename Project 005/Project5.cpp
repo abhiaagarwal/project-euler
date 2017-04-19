28c4bebad486ea94dfb4e6ce465ce20f6a19343a
Abhi Agarwal <goldcobrapro@gmail.com>
2017-04-19 10:50:02 -0400

    Add Project 5
    It’s not very finished though.

---
+++ Project 005/Project5.cpp
@@ -0,0 +1,22 @@
+// Copyright 2017 <Abhi Agarwal>
+#include <cstdint>
+#include <iostream>
+
+int main() {
+  // There's a purely mathematical way to do this, but I don't really care.
+  constexpr size_t limit = 20;
+  bool success = false;
+  size_t i;
+  for (i = limit; !success; i++) {
+    for (size_t j = 1; j <= limit; j++) {
+      if (i % j != 0) {
+        break;
+      }
+      if (j == limit) {
+        success = true;
+      }
+    }
+  }
+  std::cout << "Number is: " << i;
+  return 0;
+}
